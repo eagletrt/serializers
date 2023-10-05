8c270c7cb8801a8e51fa4e9489ee2b14f07320b9
Davide Andreolli <dvd2003333@gmail.com>
2023-10-05 14:32:59 +0200

    Initial develop of header file generation

---
+++ src/generator/main.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import json
+import proto_utils
+import jinja2
+from proto_schema_parser import Parser, Field, Message
+from proto_schema_parser.ast import OneOf
+
+parser = argparse.ArgumentParser(description='Generate cpp code from proto schemas')
+
+parser.add_argument("proto_dir", type=str, help="directory path of .proto files")
+parser.add_argument("output_dir", type=str, help="directory path of generated files")
+
+with open("src/templates/wrapper.h.j2", "r") as f:
+    cpp_template = jinja2.Template(f.read())
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    os.path.isdir(args.proto_dir) or parser.error(f"{args.proto_dir} is not a directory")
+    os.path.isdir(args.output_dir) or parser.error(f"{args.output_dir} is not a directory")
+
+    for filename in os.listdir(args.proto_dir):
+
+        print(filename)
+
+        if not filename.endswith(".proto"):
+            continue
+
+        with open(os.path.join(args.proto_dir, filename), "r") as f:
+            schema = Parser().parse(f.read())
+            messages = list(filter(lambda e: isinstance(e, Message), schema.file_elements))
+            print(schema)
+
+            package = proto_utils.get_package(messages)
+
+            if package is not None:
+                schema.file_elements.pop(package)
+
+            for message in list(filter(lambda e: isinstance(e, Message),schema.file_elements)):
+                for field in message.elements:
+                    proto_utils.set_correct_type(field)
+
+            with open(os.path.join(args.output_dir, f"{filename}.h"), "w") as f:
+                f.write(cpp_template.render(messages=messages, package=package))
+
