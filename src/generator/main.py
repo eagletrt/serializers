a35342a7c2529ec172be9babe32b9730c6af8089
Davide Andreolli <dvd2003333@gmail.com>
2023-10-09 17:51:50 +0200

    Added enum fields

--- src/generator/main.py
+++ src/generator/main.py
@@ -4,7 +4,7 @@ import json
 import proto_utils
 import jinja2
 from proto_schema_parser import Parser, Field, Message
-from proto_schema_parser.ast import OneOf
+from proto_schema_parser.ast import OneOf, Enum
 
 parser = argparse.ArgumentParser(description='Generate cpp code from proto schemas')
 
@@ -12,13 +12,16 @@ parser.add_argument("proto_dir", type=str, help="directory path of .proto files"
 parser.add_argument("output_dir", type=str, help="directory path of generated files")
 
 with open("src/templates/wrapper.h.j2", "r") as f:
-    cpp_template = jinja2.Template(f.read())
+    h_template = jinja2.Template(f.read())
+# with open("src/templates/wrapper.cc.j2", "r") as f:
+#     cpp_template = jinja2.Template(f.read())
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     os.path.isdir(args.proto_dir) or parser.error(f"{args.proto_dir} is not a directory")
-    os.path.isdir(args.output_dir) or parser.error(f"{args.output_dir} is not a directory")
+    os.makedirs(os.path.join(args.output_dir, "inc"), exist_ok=True)
+    os.makedirs(os.path.join(args.output_dir, "src"), exist_ok=True)
 
     for filename in os.listdir(args.proto_dir):
 
@@ -28,19 +31,22 @@ if __name__ == "__main__":
             continue
 
         with open(os.path.join(args.proto_dir, filename), "r") as f:
+
             schema = Parser().parse(f.read())
+
             package = proto_utils.get_package(schema.file_elements)
             messages = list(filter(lambda e: isinstance(e, Message), schema.file_elements))
-            print(schema)
+            enums = list(filter(lambda e: isinstance(e, Enum), schema.file_elements))
 
             if package is None:
                 print("No package found for file {filename}, aborting")
                 exit(1)
 
-            for message in list(filter(lambda e: isinstance(e, Message),schema.file_elements)):
-                for field in message.elements:
+            for file_element in list(filter(lambda e: isinstance(e, Message),schema.file_elements)):
+                for field in file_element.elements:
                     proto_utils.set_correct_type(field)
 
             with open(os.path.join(args.output_dir, "inc", f"{package.name}.h"), "w") as f:
-                f.write(cpp_template.render(messages=messages, package=package))
-
+                f.write(h_template.render(messages=messages, enums=enums, package=package))
+            # with open(os.path.join(args.output_dir, "src", f"{package.name}.cpp"), "w") as f:
+            #     f.write(cpp_template.render(messages=messages, package=package))
